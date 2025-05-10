d58b1affebca92baf135cc50ab514a5368d9a917
Israel Dryer <israel.dryer@gmail.com>
2025-05-09 21:50:16 -0400

    simplify event architecture; add styling types and components.

--- examples/main.py
+++
@@ -1,9 +0,0 @@
-from viewforge.ui import Text, Stack
-
-def build():
-    return [
-        Stack([
-            Text("Hello from main.py!"),
-            Text("This is my updated file and it is great!")
-        ])
-    ]
